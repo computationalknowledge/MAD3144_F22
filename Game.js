e3019222285de0e96b568200e1d331dec67e2a5f
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2022-12-08 09:08:47 -0500

    Update and rename randoms.js to Game.js

---
+++ Game.js
@@ -0,0 +1,20 @@
+function decider(){
+    var result = Math.floor(Math.random() * 11);
+    if (result > 5){return true;}
+    return false;
+}
+
+const myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+
+////////////////////////////
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Who are you?', name => {
+  console.log(`Hey there ${name}!`);
+  readline.close();
+});
--- randoms.js
+++
@@ -1,9 +0,0 @@
-function decider(){
-    var result = Math.floor(Math.random() * 11);
-    if (result > 5){return true;}
-    return false;
-}
-
-const myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
-
